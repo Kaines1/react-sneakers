73e7980166bc1799d62f75f9366b3eb9505dfc76
Dmytro <dimaret87@gmail.com>
2022-05-07 01:30:02 +0300

    some react things 1

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
-function Card() {
+import styles from "./Card.module.scss"
+
+function Card(props) {
+
+
   return (
-    <div className="card">
-      <div className="favourite">
+    <div className={styles.card}>
+      <div className={styles.favourite}>
         <img src="/img/heart-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" />
-      <h5> Мужские Кроссовки Nike Blazer Mid Suede </h5>
+      <img width={133} height={112} src={props.imageUrl} />
+      <h5> {props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column ">
-          <span> Цена: </span> <b> 12 999 руб. </b>
+          <span> Цена: </span> <b> {props.price} руб. </b>
         </div>
-        <button className="button">
+        <button onClick={()=>{alert(1)}} className="button">
           <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
         </button>
       </div>
